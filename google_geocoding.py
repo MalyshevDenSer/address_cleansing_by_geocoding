32e2ee4a6b5f6f8bedab974ea86462b8c1e9a8fa
Denis Malyshev <maldenser@gmail.com>
2023-02-01 23:00:02 +0300

    reading all adressess

--- google_geocoding.py
+++ google_geocoding.py
@@ -2,11 +2,20 @@ from geopy import GoogleV3
 import os
 from dotenv import load_dotenv
 from pprint import pprint
+import openpyxl
 
 load_dotenv()
 
 
-google_geocoder = GoogleV3(api_key=os.getenv('GOOGLE_V3_API'), domain='maps.google.ru')
-random_place = 'проспект Ленина, 18Б Центр район, Петрозаводск, 185035'
-geocoded = google_geocoder.geocode('проспект Ленина, 18Б Центр район, Петрозаводск, 185035')
-print(geocoded.raw['address_components'][0]['types'])
+wb_obj = openpyxl.load_workbook(os.getenv('PATH_TO_FILE'))
+sheet_obj = wb_obj.active
+
+source_row = sheet_obj['A']
+for i, cell in enumerate(source_row[1:], start=1):
+    place = cell.value
+    print(place)
+
+# google_geocoder = GoogleV3(api_key=os.getenv('GOOGLE_V3_API'), domain='maps.google.ru')
+# # random_place = 'проспект Ленина, 18Б Центр район, Петрозаводск, 185035'
+# geocoded = google_geocoder.geocode('проспект Ленина, 18Б Центр район, Петрозаводск, 185035')
+# pprint(geocoded.raw['address_components'])
