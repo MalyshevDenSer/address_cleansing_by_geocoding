fc23c574b5f3e3b25e3b4ec8d57b4355e7c262bd
Denis Malyshev <maldenser@gmail.com>
2023-02-02 11:43:39 +0300

    finding country in each address

--- google_geocoding.py
+++ google_geocoding.py
@@ -9,13 +9,16 @@ load_dotenv()
 
 wb_obj = openpyxl.load_workbook(os.getenv('PATH_TO_FILE'))
 sheet_obj = wb_obj.active
-
 source_row = sheet_obj['A']
+google_geocoder = GoogleV3(api_key=os.getenv('GOOGLE_V3_API'), domain='maps.google.ru')
+
 for i, cell in enumerate(source_row[1:], start=1):
     place = cell.value
-    print(place)
+    geocoded = google_geocoder.geocode(place)
+    country = region = city = street = house_number = post_code = position = '!!!ERROR!!!'
+    if geocoded is not None:
+        for numb, info in enumerate(geocoded.raw['address_components']):
+            if 'country' in info['types']:
+                country = info['long_name']
 
-# google_geocoder = GoogleV3(api_key=os.getenv('GOOGLE_V3_API'), domain='maps.google.ru')
-# # random_place = 'проспект Ленина, 18Б Центр район, Петрозаводск, 185035'
-# geocoded = google_geocoder.geocode('проспект Ленина, 18Б Центр район, Петрозаводск, 185035')
-# pprint(geocoded.raw['address_components'])
+    print(country)
