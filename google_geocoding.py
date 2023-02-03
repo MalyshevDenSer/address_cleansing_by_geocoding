72710339f15f99bddd2272ca1a483ea78737396e
Denis Malyshev <maldenser@gmail.com>
2023-02-03 16:31:59 +0300

    parsing data to different columns

--- google_geocoding.py
+++ google_geocoding.py
@@ -7,10 +7,24 @@ from string import ascii_uppercase
 
 load_dotenv()
 
-
 wb_obj = openpyxl.load_workbook(os.getenv('PATH_TO_FILE'))
 sheet_obj = wb_obj.active
 source_row = sheet_obj[ascii_uppercase[0]]
+country_row = sheet_obj[ascii_uppercase[1]]
+country_row[0].value = 'Страна'
+region_row = sheet_obj[ascii_uppercase[2]]
+region_row[0].value = 'Регион'
+city_row = sheet_obj[ascii_uppercase[3]]
+city_row[0].value = 'Город'
+street_row = sheet_obj[ascii_uppercase[4]]
+street_row[0].value = 'Улица'
+house_row = sheet_obj[ascii_uppercase[5]]
+house_row[0].value = 'Дом'
+postcode_row = sheet_obj[ascii_uppercase[6]]
+postcode_row[0].value = 'Индекс'
+position_row = sheet_obj[ascii_uppercase[7]]
+position_row[0].value = 'Координаты'
+
 google_geocoder = GoogleV3(api_key=os.getenv('GOOGLE_V3_API'), domain='maps.google.ru')
 
 for i, cell in enumerate(source_row[1:], start=1):
@@ -35,6 +49,14 @@ for i, cell in enumerate(source_row[1:], start=1):
             position = str(geocoded.raw['geometry']['location']['lat']) + ' ' + str(
                 geocoded.raw['geometry']['location']['lng'])
 
+    country_row[i].value = country
+    position_row[i].value = position
+    city_row[i].value = city
+    region_row[i].value = region
+    house_row[i].value = house_number
+    street_row[i].value = street
+    postcode_row[i].value = post_code
+
     raw_place = [country, region, city, street, house_number, post_code, position]
-    cell.value = ' '.join(raw_place)
+    print(' '.join(raw_place))
 wb_obj.save('result.xlsx')
