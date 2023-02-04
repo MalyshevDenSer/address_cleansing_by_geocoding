21f027dd9eac7cbdbc67c2e84397f273e723e50d
Denis Malyshev <maldenser@gmail.com>
2023-02-04 18:13:55 +0300

    erasing comments, united env var in main

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import os
+
 from dotenv import load_dotenv
 
-import os
-import worksheet, google_geocoding
+import google_geocoding
+import worksheet
 
 load_dotenv()
-wb = worksheet.open_wb(os.getenv('PATH_TO_FILE'))
+
+GOOGLE_V3_API = os.getenv('GOOGLE_V3_API')
+PATH_TO_FILE = os.getenv('PATH_TO_FILE')
+
+wb = worksheet.open_wb(PATH_TO_FILE)
 address_sheet = wb.active
 google_geocoding.google_geocode(address_sheet)
 worksheet.save_wb(wb)
