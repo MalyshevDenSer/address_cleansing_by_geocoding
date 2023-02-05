c1db31ae4394f45edb8ded4bb356e2b3e04cdf64
Denis Malyshev <maldenser@gmail.com>
2023-02-05 23:56:49 +0300

    minor fixes

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ PATH_TO_FILE = os.getenv('PATH_TO_FILE')
 
 wb = worksheet.open_wb(PATH_TO_FILE)
 address_sheet = wb.active
-google_geocoding.google_geocode(address_sheet)
+google_geocoding.google_geocode(address_sheet, GOOGLE_V3_API)
 worksheet.save_wb(wb)
