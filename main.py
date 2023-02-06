24aea5037fdc1a80dd6a8fa74b6034a8737f39f8
Denis Malyshev <maldenser@gmail.com>
2023-02-06 23:35:55 +0300

    added ability to choose yandex or google geocoder

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ load_dotenv()
 GOOGLE_V3_API = os.getenv('GOOGLE_V3_API')
 YANDEX_API = os.getenv('YANDEX_API')
 PATH_TO_FILE = os.getenv('PATH_TO_FILE')
+MAP_GEOCODER = os.getenv('MAP_GEOCODER')
 
 
 wb = worksheet.open_wb(PATH_TO_FILE)
 address_sheet = wb.active
-# google_geocoding.google_geocode(address_sheet, GOOGLE_V3_API)
-yandex_geocoding.yandex_geocode(address_sheet, YANDEX_API)
+if MAP_GEOCODER == 'GOOGLE':
+    google_geocoding.google_geocode(address_sheet, GOOGLE_V3_API)
+elif MAP_GEOCODER == 'YANDEX':
+    yandex_geocoding.yandex_geocode(address_sheet, YANDEX_API)
+else:
+    print(f"record to .env service (YANDEX OR GOOGLE) and its api")
 worksheet.save_wb(wb)
