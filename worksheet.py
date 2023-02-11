cd5a35b7f25c8f0a10a4518e5f531d09252f2763
Denis Malyshev <maldenser@gmail.com>
2023-02-11 15:43:35 +0300

    more type annotations

--- worksheet.py
+++ worksheet.py
@@ -1,18 +1,22 @@
 import openpyxl
 from string import ascii_uppercase
+from openpyxl.workbook.workbook import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+from collections import OrderedDict
 
 
-def open_wb(path_to_file):
+def open_wb(path_to_file: str) -> Workbook:
     wb_obj = openpyxl.load_workbook(path_to_file)
     return wb_obj
 
 
-def write_in_a_row(sheet, i, parsed_info):
+def write_in_a_row(sheet: Worksheet, i: int, parsed_info: OrderedDict) -> None:
     for letter, key in enumerate(parsed_info, start=1):
         sheet[ascii_uppercase[letter]][i].value = parsed_info[key]
 
 
-def save_wb(wb):
+def save_wb(wb: Workbook) -> None:
+    print(type(wb))
     wb.save('result.xlsx')
 
 
