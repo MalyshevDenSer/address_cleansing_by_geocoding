24f5d14bab6080024b37f9bbf74a6e34a6bf9902
Denis Malyshev <maldenser@gmail.com>
2023-02-13 22:52:25 +0300

    minor

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import geocoding
 import worksheet
 from settings import geocoders, PATH_TO_FILE
 
-
-wb = worksheet.open_wb(PATH_TO_FILE)
-address_sheet = wb.active
-geocoding.geocode(address_sheet, geocoders)
-worksheet.save_wb(wb)
+if __name__ == "__main__":
+    wb = worksheet.open_wb(PATH_TO_FILE)
+    address_sheet = wb.active
+    geocoding.geocode(address_sheet, geocoders)
+    worksheet.save_wb(wb)
