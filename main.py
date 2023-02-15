daafd4eb5de9069fd43cb9312cb532724d01e57b
Denis Malyshev <maldenser@gmail.com>
2023-02-15 23:42:54 +0300

    first commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import geocoding
 import worksheet
 from settings import geocoders, PATH_TO_FILE
 
-
+# OOP
 wb = worksheet.open_wb(PATH_TO_FILE)
 address_sheet = wb.active
 geocoding.geocode(address_sheet, geocoders)
