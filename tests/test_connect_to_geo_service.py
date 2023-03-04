8d0c460582a8c980afab7cfe252f543c87e8a724
Denis Malyshev <maldenser@gmail.com>
2023-03-04 21:10:30 +0300

    struggling with russian symbols - added pytest.ini

--- tests/test_connect_to_geo_service.py
+++ tests/test_connect_to_geo_service.py
@@ -1,11 +1,38 @@
+# -*- coding: utf-8 -*-
+
 import pytest
 from settings import geocoders
 
-DEFAULT_TEST_ADDRESS = 'Скатертный переулок, 2/7, Москва, Россия'
-DEFAULT_TEST_ADDRESS_CITY = 'Москва'
-check_connection_params = [pytest.param(geocoder['engine'], id=geocoder['type']) for geocoder in geocoders]
+DEFAULT_TEST_ADDRESSES = [
+    # 'Tverskaya St, 1, Moscow, Russia'
+    'Скатертный переулок, 2/7, Москва, Россия',
+    'Аксенова 18, Обнинск, Россия',
+    'улица Трудовые Резервы, 3, Новомосковск, Россия'
+]
+
+DEFAULT_TEST_ADDRESSES_CITY = [
+    # 'Moscow'
+    'Москва',
+    'Обнинск',
+    'Новомосковск'
+]
+
+DEFAULT = [
+    ('Москва', 'Скатертный переулок, 2/7, Москва, Россия'),
+    ('Обнинск', 'Аксенова 18, Обнинск, Россия'),
+    ('Новомосковск', 'улица Трудовые Резервы, 3, Новомосковск, Россия')
+]
 
 
-@pytest.mark.parametrize('geocoder', check_connection_params)
-def test_check_connection(geocoder):
-    assert DEFAULT_TEST_ADDRESS_CITY in str(geocoder.geocode(DEFAULT_TEST_ADDRESS))
+# check_connection_params = [pytest.param(geocoder['engine'], id=geocoder['type']) for geocoder in geocoders]
+
+
+@pytest.mark.parametrize(
+    'city, address',
+    DEFAULT,
+)
+def test_check_connection_google(city, address):
+    assert city in str(geocoders[0]['engine'].geocode(address))
+
+
+# def test_google_keys_nested_dict(geocoder)
