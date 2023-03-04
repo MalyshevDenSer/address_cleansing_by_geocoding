df0fa4a1a1a6210fbd29ea5e199ada4156b86487
Denis Malyshev <maldenser@gmail.com>
2023-03-04 21:46:10 +0300

    refactored

--- tests/test_connect_to_geo_service.py
+++ tests/test_connect_to_geo_service.py
@@ -3,36 +3,17 @@
 import pytest
 from settings import geocoders
 
-DEFAULT_TEST_ADDRESSES = [
-    # 'Tverskaya St, 1, Moscow, Russia'
-    'Скатертный переулок, 2/7, Москва, Россия',
-    'Аксенова 18, Обнинск, Россия',
-    'улица Трудовые Резервы, 3, Новомосковск, Россия'
-]
-
-DEFAULT_TEST_ADDRESSES_CITY = [
-    # 'Moscow'
-    'Москва',
-    'Обнинск',
-    'Новомосковск'
-]
-
-DEFAULT = [
-    ('Москва', 'Скатертный переулок, 2/7, Москва, Россия'),
-    ('Обнинск', 'Аксенова 18, Обнинск, Россия'),
-    ('Новомосковск', 'улица Трудовые Резервы, 3, Новомосковск, Россия')
-]
-
-
-# check_connection_params = [pytest.param(geocoder['engine'], id=geocoder['type']) for geocoder in geocoders]
+DEFAULT = (
+    ['Москва', 'Скатертный переулок, 2/7, Москва, Россия'],
+    ['Обнинск', 'Аксенова 18, Обнинск, Россия'],
+    ['Новомосковск', 'улица Трудовые Резервы, 3, Новомосковск, Россия']
+)
 
 
 @pytest.mark.parametrize(
-    'city, address',
-    DEFAULT,
+    'city, address, geocoder',
+    [list(DEFAULT[0]) + [geocoder['engine']] for geocoder in geocoders],
+    ids=[geocoder['type'] for geocoder in geocoders]
 )
-def test_check_connection_google(city, address):
-    assert city in str(geocoders[0]['engine'].geocode(address))
-
-
-# def test_google_keys_nested_dict(geocoder)
+def test_check_connection(city, address, geocoder):
+    assert city in str(geocoder.geocode(address))
