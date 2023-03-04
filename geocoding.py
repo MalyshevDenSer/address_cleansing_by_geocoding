0d7ec64d7ef485a7d74874134e9e21d4c2bc7aa5
Denis Malyshev <maldenser@gmail.com>
2023-03-05 00:56:39 +0300

    splitiong parsing function in order to create tests

--- geocoding.py
+++ geocoding.py
@@ -5,7 +5,10 @@ from typing import Union
 from openpyxl.worksheet.worksheet import Worksheet
 from geopy.geocoders.yandex import Yandex
 from geopy.geocoders.google import GoogleV3
+from geopy.location import Location
+from geopy.exc import GeopyError
 from pprint import pprint
+from time import sleep
 
 
 def find_address_components(info: dict, keys: list) -> list:
@@ -16,12 +19,23 @@ def find_address_components(info: dict, keys: list) -> list:
     return info
 
 
-def parsing(geocoder: dict[str, Union[Yandex, GoogleV3, dict, str]], place: str) -> OrderedDict:
-    geocoded = geocoder['engine'].geocode(place)
-    keys = geocoder['keys']
+def connect_to_api(geocoder: Union[Yandex, GoogleV3], place: str) -> Union[dict, None]:
+    """Connectivity problems sometimes occur, so just in case, the script expects exceptions"""
+    try:
+        geocoded = geocoder.geocode(place)
+        return geocoded
+    except GeopyError as err:
+        print('!' * 50)
+        print(f'{type(err).__name__} is occured. Sleeping for 5 seconds.')
+        sleep(5)
+        print(f'Continuing. The address {place} is skipped')
+        return None
+
+
+def parsing_raw_data(data, keys, geocoder_type) -> OrderedDict:
     country = region = city = street = house_number = postal_code = None
-    if geocoded is not None:
-        for info in find_address_components(geocoded.raw, keys['nested_dict']):
+    if data is not None:
+        for info in find_address_components(data.raw, keys['nested_dict']):
             item = info[keys['item']]
             value = info[keys['value']]
             if keys['country'] in item:
@@ -34,8 +48,8 @@ def parsing(geocoder: dict[str, Union[Yandex, GoogleV3, dict, str]], place: str)
                 house_number = value
             if keys['street'] in item:
                 street = value
-            postal_code = find_address_components(geocoded.raw, keys.get('postal_code'))
-            if geocoder['type'] == 'google' and postal_code is not None:
+            postal_code = find_address_components(data.raw, keys.get('postal_code'))
+            if geocoder_type == 'google' and postal_code is not None:
                 postal_code = postal_code[-6:]
 
     parsed_info = OrderedDict([
@@ -52,11 +66,11 @@ def parsing(geocoder: dict[str, Union[Yandex, GoogleV3, dict, str]], place: str)
 
 def geocode(sheet: Worksheet, geocoders: list[dict]) -> None:
     source_column = sheet[ascii_uppercase[0]]
-
     for i, cell in enumerate(source_column[1:], start=1):
         place = cell.value
         for geocoder in geocoders:
-            parsed_info = parsing(geocoder, place)
-            if all(parsed_info.values()):
-                worksheet.write_in_a_row(sheet, i, parsed_info)
+            raw_data = connect_to_api(geocoder['engine'], place)
+            parsed_data = parsing_raw_data(raw_data, geocoder['keys'], geocoder['type'])
+            if all(parsed_data.values()):
+                worksheet.write_in_a_row(sheet, i, parsed_data)
                 break
