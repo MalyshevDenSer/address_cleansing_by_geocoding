e685ec6a454757db4c0660e9418376f5884e4c0a
Denis Malyshev <maldenser@gmail.com>
2023-03-05 18:55:03 +0300

    pytest apis

--- tests/test_connect_to_geo_service.py
+++ tests/test_connect_to_geo_service.py
@@ -2,6 +2,10 @@
 
 import pytest
 from settings import geocoders
+from geocoding import connect_to_api
+from geopy.exc import GeocoderQueryError, GeocoderInsufficientPrivileges, GeopyError
+from urllib.error import HTTPError
+from settings import GOOGLE_V3_API
 
 DEFAULT = (
     ['Москва', 'Скатертный переулок, 2/7, Москва, Россия'],
@@ -11,9 +15,16 @@ DEFAULT = (
 
 
 @pytest.mark.parametrize(
-    'city, address, geocoder',
-    [list(DEFAULT[0]) + [geocoder['engine']] for geocoder in geocoders],
+    'city, address, g_type, geocoder',
+    [list(DEFAULT[0]) + [geocoder['type']] + [geocoder['engine']] for geocoder in geocoders],
     ids=[geocoder['type'] for geocoder in geocoders]
 )
-def test_check_connection(city, address, geocoder):
-    assert city in str(geocoder.geocode(address))
+def test_check_connection(city, address, g_type, geocoder):
+    message = f'Wrong {g_type} API key is provided or it is blocked by service'
+    try:
+        connect_to_api(geocoder, g_type, address)
+    except GeocoderInsufficientPrivileges:
+        assert False, message
+    except GeocoderQueryError:
+        assert False, message
+
